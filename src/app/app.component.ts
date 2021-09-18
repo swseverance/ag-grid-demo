e3e0c0ed572cce58b4dd9795762f08b2ff7be054
Sam Severance <swseverance@gmail.com>
2021-09-18 08:47:02 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-app';
+  @ViewChild('agGrid') agGrid: AgGridAngular;
+
+  defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+  };
+
+  columnDefs: ColDef[] = [
+    { field: 'make', rowGroup: true }, // group data by make
+    { field: 'price' },
+  ];
+
+  autoGroupColumnDef: ColDef = {
+    headerName: 'Model',
+    field: 'model',
+    cellRenderer: 'agGroupCellRenderer',
+    cellRendererParams: {
+      checkbox: true,
+    },
+  };
+
+  rowData$: Observable<any[]> = this.http.get<any[]>(
+    'https://www.ag-grid.com/example-assets/row-data.json'
+  );
+
+  constructor(private http: HttpClient) {}
+
+  getSelectedRows() {
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map((node) => {
+      if (node.groupData) return { make: node.key, model: 'Group' };
+      return node.data;
+    });
+    const str = selectedData
+      .map((node) => `${node.make} ${node.model}`)
+      .join(', ');
+    alert(str);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
+import 'ag-grid-enterprise';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
